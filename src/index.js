731a62e491589cc3c7bfaf86c2a4f85111437a96
Melissa <melissa.scmi@gmail.com>
2021-08-30 19:38:58 -0700

    add 1st test & pass 1st test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import Triangle from './triangle.js';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
 $(document).ready(function() {
   $('#triangle-checker-form').submit(function(event) {
     event.preventDefault();
